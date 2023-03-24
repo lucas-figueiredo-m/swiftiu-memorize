30926f4d3877d68a66d08ea9902d1988e086708f
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2023-03-24 17:00:55 -0300

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Lucas Martins Figueiredo on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
